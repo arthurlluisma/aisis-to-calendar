c64b20183709fc026a3f6dfdde784a199006db0c
Arthur Lluisma <ajclluisma@gmail.com>
2025-12-26 21:59:05 +0800

    refactor: refactor event listeners to use the same function

--- popup/popup.js
+++ popup/popup.js
@@ -1,85 +1,122 @@
 document.addEventListener("DOMContentLoaded", function () {
-  const Days = Object.freeze({
-    "S": 0,
-    "M": 1,
-    "T": 2,
-    "W": 3,
-    "TH": 4,
-    "F": 5,
-    "SAT": 6
-  }) 
-
-  const ICSDays = Object.freeze({
-    "S": "SU",
-    "M": "MO",
-    "T": "TU",
-    "W": "WE",
-    "TH": "TH",
-    "F": "FR",
-    "SAT": "SA"
-  })
-
   const ICSbutton = document.querySelector("button#ics");
   const CSVbutton = document.querySelector("button#csv");
 
-  ICSbutton.addEventListener("click", async function () {
-    try {
-      const [tab] = await chrome.tabs.query({
-        active: true,
-        currentWindow: true,
-      });
+  ICSbutton.addEventListener("click", convertCalendar.bind(null, "ICS"));
 
-      const results = await chrome.scripting.executeScript({
-        target: { tabId: tab.id },
-        function: extractTableAfterComment,
-      });
+  CSVbutton.addEventListener("click", convertCalendar.bind(null, "CSV"));
+});
 
-      if (results && results[0] && results[0].result) {
-        const tableHTML = results[0].result;
+const Days = Object.freeze({
+  "S": 0,
+  "M": 1,
+  "T": 2,
+  "W": 3,
+  "TH": 4,
+  "F": 5,
+  "SAT": 6
+}) 
 
-        if (tableHTML) {
-          console.log("Found table:", tableHTML);
+const ICSDays = Object.freeze({
+  "S": "SU",
+  "M": "MO",
+  "T": "TU",
+  "W": "WE",
+  "TH": "TH",
+  "F": "FR",
+  "SAT": "SA"
+})
 
-          const parser = new DOMParser();
-          const doc = parser.parseFromString(tableHTML, "text/html");
-          const tableElement = doc.querySelector("table");
+async function convertCalendar(whichButton) {
+  try {
+    const [tab] = await chrome.tabs.query({
+      active: true,
+      currentWindow: true,
+    });
 
-          console.log("Table element:", tableElement);
+    const results = await chrome.scripting.executeScript({
+      target: { tabId: tab.id },
+      function: extractTableAfterComment,
+    });
 
-          const tbody = tableElement.querySelector("tbody");
-          if (tbody) {
-            const rows = Array.from(tbody.rows);
-            rows.shift();
-            rows.pop();
+    if (results && results[0] && results[0].result) {
+      const tableHTML = results[0].result;
 
-            let events = [];
+      if (tableHTML) {
+        console.log("Found table:", tableHTML);
 
-            for (const row of rows) {
-              const courseCode = row.cells[0].innerText.trim();
-              const section = row.cells[1].innerText.trim();
-              const instructor = row.cells[3].innerText.trim();
-              const schedule = row.cells[4].innerText.trim();
+        const parser = new DOMParser();
+        const doc = parser.parseFromString(tableHTML, "text/html");
+        const tableElement = doc.querySelector("table");
 
-              if (schedule.includes("TBA")) continue;
+        console.log("Table element:", tableElement);
 
-              let dateAndVenue = schedule.split("(")[0];
-              let dayAndTime = dateAndVenue.split("/");
-              let timeSplit = dayAndTime[0].split(" ")[1].split("-");
-              let day = dayAndTime[0].split(" ")[0];
-              let startTime = timeSplit[0];
-              let endTime = timeSplit[1];
-              let venue = dayAndTime[1];
+        const tbody = tableElement.querySelector("tbody");
+        if (tbody) {
+          const rows = Array.from(tbody.rows);
+          rows.shift();
+          rows.pop();
 
-              if (day.includes("-")) {
-                let dayValues = day.split("-");
-                let firstDay = dayValues[0];
-                let secondDay = dayValues[1];
+          let events = [];
 
-                let firstDayDate = getFirstDayOccurrence(2026, 0, Days[firstDay]);
-                let secondDayDate = getFirstDayOccurrence(2026, 0, Days[secondDay]);
+          for (const row of rows) {
+            const courseCode = row.cells[0].innerText.trim();
+            const section = row.cells[1].innerText.trim();
+            const instructor = row.cells[3].innerText.trim();
+            const schedule = row.cells[4].innerText.trim();
 
-                console.log({ firstDayDate, secondDayDate, startTime, endTime, venue });
+            if (schedule.includes("TBA")) continue;
 
+            let dateAndVenue = schedule.split("(")[0];
+            let dayAndTime = dateAndVenue.split("/");
+            let timeSplit = dayAndTime[0].split(" ")[1].split("-");
+            let day = dayAndTime[0].split(" ")[0];
+            let startTime = timeSplit[0];
+            let endTime = timeSplit[1];
+            let venue = dayAndTime[1];
+
+            if (day.includes("-")) {
+              let dayValues = day.split("-");
+              let firstDay = dayValues[0];
+              let secondDay = dayValues[1];
+
+              let firstDayDate = getFirstDayOccurrence(2026, 0, Days[firstDay]);
+              let secondDayDate = getFirstDayOccurrence(2026, 0, Days[secondDay]);
+
+              console.log({ firstDayDate, secondDayDate, startTime, endTime, venue });
+
+              const lastDayOfClasses = new Date(2026, 5, 1);
+
+              if (whichButton === "CSV") {
+                while (firstDayDate < lastDayOfClasses) {
+                  events.push({
+                    "Subject": courseCode,
+                    "Start Date": firstDayDate.toLocaleDateString("en-CA"),
+                    "Start Time": startTime,
+                    "End Date": firstDayDate.toLocaleDateString("en-CA"),
+                    "End Time": endTime,
+                    "Description": `Section: ${section}\nInstructor: ${instructor}`,
+                    "Location": venue.trim(),
+                    "All Day Event": "False",
+                    "Private": "False"
+                  })
+                  firstDayDate.setDate(firstDayDate.getDate() + 7);
+                }
+                while (secondDayDate < lastDayOfClasses) {
+                  events.push({
+                    "Subject": courseCode,
+                    "Start Date": secondDayDate.toLocaleDateString("en-CA"),
+                    "Start Time": startTime,
+                    "End Date": secondDayDate.toLocaleDateString("en-CA"),
+                    "End Time": endTime,
+                    "Description": `Section: ${section}\nInstructor: ${instructor}`,
+                    "Location": venue.trim(),
+                    "All Day Event": "False",
+                    "Private": "False"
+                  })
+                  secondDayDate.setDate(secondDayDate.getDate() + 7);
+                }
+              } else {
                 events.push({
                   summary: courseCode,
                   start: `${firstDayDate.getFullYear()}${String(firstDayDate.getMonth()+1).padStart(2, '0')}${String(firstDayDate.getDate()).padStart(2, '0')}T${startTime.replace(":", "")}00`,
@@ -89,11 +126,31 @@ document.addEventListener("DOMContentLoaded", function () {
                   byday: `${ICSDays[firstDay]},${ICSDays[secondDay]}`,
                   endString: "20260601"
                 })
+              }
+              
+            } else {
+              let dayDate = getFirstDayOccurrence(2026, 0, Days[day]);
+
+              console.log({ dayDate, startTime, endTime, venue });
+
+              const lastDayOfClasses = new Date(2026, 5, 1);
+
+              if (whichButton === "CSV") {
+                while (dayDate < lastDayOfClasses) {
+                  events.push({
+                    "Subject": courseCode,
+                    "Start Date": dayDate.toLocaleDateString("en-CA"),
+                    "Start Time": startTime,
+                    "End Date": dayDate.toLocaleDateString("en-CA"),
+                    "End Time": endTime,
+                    "Description": `Section: ${section}\nInstructor: ${instructor}`,
+                    "Location": venue.trim(),
+                    "All Day Event": "False",
+                    "Private": "False"
+                  })
+                  dayDate.setDate(dayDate.getDate() + 7);
+                }
               } else {
-                let dayDate = getFirstDayOccurrence(2026, 0, Days[day]);
-
-                console.log({ dayDate, startTime, endTime, venue });
-
                 events.push({
                   summary: courseCode,
                   start: `${dayDate.getFullYear()}${String(dayDate.getMonth()+1).padStart(2, '0')}${String(dayDate.getDate()).padStart(2, '0')}T${startTime.replace(":", "")}00`,
@@ -105,147 +162,27 @@ document.addEventListener("DOMContentLoaded", function () {
                 })
               }
             }
+          }
 
+          if (whichButton === "CSV") {
+            const csvData = convertToCSV(events);
+            downloadCSV(csvData, "schedule.csv");
+          } else {
             const icsData = convertToICS(events);
             downloadICS(icsData, "schedule.ics");
           }
-
-          alert("Conversion successful! Check your downloads for the ICS file.");
-        } else {
-          alert("Sorry, but there was an issue encountering the conversion. Please ensure you are on the correct page with the schedule table.");
         }
+
+        alert(`Conversion successful! Check your downloads for the ${whichButton} file.`);
+      } else {
+        alert("Sorry, but there was an issue encountering the conversion. Please ensure you are on the correct page with the schedule table.");
       }
-    } catch (error) {
-      console.error("Error:", error);
-      alert("Error: " + error.message);
     }
-  });
-
-  CSVbutton.addEventListener("click", async function () {
-    try {
-      const [tab] = await chrome.tabs.query({
-        active: true,
-        currentWindow: true,
-      });
-
-      const results = await chrome.scripting.executeScript({
-        target: { tabId: tab.id },
-        function: extractTableAfterComment,
-      });
-
-      if (results && results[0] && results[0].result) {
-        const tableHTML = results[0].result;
-
-        if (tableHTML) {
-          console.log("Found table:", tableHTML);
-
-          const parser = new DOMParser();
-          const doc = parser.parseFromString(tableHTML, "text/html");
-          const tableElement = doc.querySelector("table");
-
-          console.log("Table element:", tableElement);
-
-          const tbody = tableElement.querySelector("tbody");
-          if (tbody) {
-            const rows = Array.from(tbody.rows);
-            rows.shift();
-            rows.pop();
-
-            let data = [];
-
-            for (const row of rows) {
-              const courseCode = row.cells[0].innerText.trim();
-              const section = row.cells[1].innerText.trim();
-              const instructor = row.cells[3].innerText.trim();
-              const schedule = row.cells[4].innerText.trim();
-
-              if (schedule.includes("TBA")) continue;
-
-              let dateAndVenue = schedule.split("(")[0];
-              let dayAndTime = dateAndVenue.split("/");
-              let timeSplit = dayAndTime[0].split(" ")[1].split("-");
-              let day = dayAndTime[0].split(" ")[0];
-              let startTime = timeSplit[0];
-              let endTime = timeSplit[1];
-              let venue = dayAndTime[1];
-
-              if (day.includes("-")) {
-                let dayValues = day.split("-");
-                let firstDay = dayValues[0];
-                let secondDay = dayValues[1];
-
-                let firstDayDate = getFirstDayOccurrence(2026, 0, Days[firstDay]);
-                let secondDayDate = getFirstDayOccurrence(2026, 0, Days[secondDay]);
-
-                console.log({ firstDayDate, secondDayDate, startTime, endTime, venue });
-
-                const lastDayOfClasses = new Date(2026, 5, 1);
-                while (firstDayDate < lastDayOfClasses) {
-                  data.push({
-                    "Subject": courseCode,
-                    "Start Date": firstDayDate.toLocaleDateString("en-CA"),
-                    "Start Time": startTime,
-                    "End Date": firstDayDate.toLocaleDateString("en-CA"),
-                    "End Time": endTime,
-                    "Description": `Section: ${section}\nInstructor: ${instructor}`,
-                    "Location": venue.trim(),
-                    "All Day Event": "False",
-                    "Private": "True"
-                  })
-                  firstDayDate.setDate(firstDayDate.getDate() + 7);
-                }
-                while (secondDayDate < lastDayOfClasses) {
-                  data.push({
-                    "Subject": courseCode,
-                    "Start Date": secondDayDate.toLocaleDateString("en-CA"),
-                    "Start Time": startTime,
-                    "End Date": secondDayDate.toLocaleDateString("en-CA"),
-                    "End Time": endTime,
-                    "Description": `Section: ${section}\nInstructor: ${instructor}`,
-                    "Location": venue.trim(),
-                    "All Day Event": "False",
-                    "Private": "True"
-                  })
-                  secondDayDate.setDate(secondDayDate.getDate() + 7);
-                }
-              } else {
-                let dayDate = getFirstDayOccurrence(2026, 0, Days[day]);
-
-                console.log({ dayDate, startTime, endTime, venue });
-
-                const lastDayOfClasses = new Date(2026, 5, 1);
-                while (dayDate < lastDayOfClasses) {
-                  data.push({
-                    "Subject": courseCode,
-                    "Start Date": dayDate.toLocaleDateString("en-CA"),
-                    "Start Time": startTime,
-                    "End Date": dayDate.toLocaleDateString("en-CA"),
-                    "End Time": endTime,
-                    "Description": `Section: ${section}\nInstructor: ${instructor}`,
-                    "Location": venue.trim(),
-                    "All Day Event": "False",
-                    "Private": "True"
-                  })
-                  dayDate.setDate(dayDate.getDate() + 7);
-                }
-              }
-            }
-
-            const csvData = convertToCSV(data);
-            downloadCSV(csvData, "schedule.csv");
-          }
-
-          alert("Conversion successful! Check your downloads for the CSV file.");
-        } else {
-          alert("Sorry, but there was an issue encountering the conversion. Please ensure you are on the correct page with the schedule table.");
-        }
-      }
-    } catch (error) {
-      console.error("Error:", error);
-      alert("Error: " + error.message);
-    }
-  });
-});
+  } catch (error) {
+    console.error("Error:", error);
+    alert("Error: " + error.message);
+  }
+}
 
 function downloadICS(icsData, filename) {
   const blob = new Blob([icsData], { type: "text/calendar;charset=utf-8" });
